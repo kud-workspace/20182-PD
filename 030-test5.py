a2e4d7228fb674b2f679c5f95c714cb9628b1cff
KudDroid <droid@kud.space>
2019-03-25 20:28:09 +0800

    kurang kerjaan v2

--- 030-test5.py
+++ 030-test5.py
@@ -1,44 +1,61 @@
 #!/usr/bin/env python3
 
-def put(a, b, c):
-    setengah = 0.5
+# input/output
+def kalkulasi_operasi(a, b, c):
+    setengah = 0.5 # luas segitiga = 1/2 luas segi empat
+    persegi = a * b # operasi pertama
+    segitiga = b * c * 0.5 # operasi kedua
+    hasil = c * a + b # operasi ketiga
 
-    print("Kalau a adalah panjang dan b adalah lebar maka luas persegi yaitu %d x %d = %d" %(a, b, a * b))
-    print("Kalau b adalah panjang dan c adalah tinggi maka segitiga yaitu %d x %d x %1.1f = %d" %(b, c, setengah, b * c * setengah))
-    print("a=%d b=%d c=%d maka (c x a) + b= (%d x %d) + %d = %d" %(a, b, c, c, a, b, c * a + b))
+    print("Kalau a adalah panjang dan b adalah lebar maka luas persegi yaitu %d x %d = %d" %(a, b, persegi))
+    print("Kalau b adalah panjang dan c adalah tinggi maka segitiga yaitu %d x %d x %1.1f = %d" %(b, c, setengah, segitiga))
+    print("a=%d b=%d c=%d maka (c x a) + b= (%d x %d) + %d = %d" %(a, b, c, c, a, b, hasil))
 
-def kondisional_if(kota, harga, tiket, tujuan, bayar):
+# kondisional if
+def bayar_tujuan(kota, harga, tiket, tujuan, bayar):
     print("Total Bayar Tujuan %s adalah" %(kota))
-    if bayar == 1:
-        print("Tiket Reguler ->", "%d x %d = %d" %(tiket, harga, tiket * harga))
-    elif bayar == 2:
-        print("Tiket Promo ->", "(%d x %d)-(%d x %d x 0.1) = %d" %(tiket, harga, tiket, harga, tiket * harga - tiket * harga * 0.1))
-    elif bayar == 3:
-        print("Tiket Hari Besar ->", "(%d x %d)+(%d x %d x 0.05) = %d" %(tiket, harga, tiket, harga, tiket * harga + tiket * harga * 0.05))
+    jumlah = tiket * harga # harga normal
 
-def berulang(kota, harga):
+    if bayar == 1: # Reguler
+        print("Tiket Reguler ->", "%d x %d = %d" %(tiket, harga, jumlah))
+
+    elif bayar == 2: # Diskon 10%
+        diskon = 0.1 # 10%
+        jumlah = jumlah - jumlah * diskon # harga setelah diskon
+
+        print("Tiket Promo ->", "(%d x %d)-(%d x %d x %1.1f) = %d" %(tiket, harga, tiket, harga, diskon, jumlah))
+
+    elif bayar == 3: # Tuslah 5%
+        tuslah = 0.05 # 5%
+        jumlah = jumlah + jumlah * tuslah # harga setelah tuslah
+
+        print("Tiket Hari Besar ->", "(%d x %d)+(%d x %d x %1.2f) = %d" %(tiket, harga, tiket, harga, tuslah, jumlah))
+
+# bilangan berulang
+def harga_tiket(kota, harga):
     print("TIKET TUJUAN %s" %(kota))
     print("Jumlah	  Reguler	Promo		Hari Besar")
-    for jumlah in range(1, 11):
+    for jumlah in range(1, 11): # 1 - 10
         print(" %d	%d		 %d		    %d" %(jumlah, jumlah * harga, jumlah * harga - jumlah * harga * 0.1, jumlah * harga + jumlah * harga * 0.05))
 
+# fungsi unified
 def proses_info(tujuan, tiket, bayar):
-    if tujuan == 1:
+    if tujuan == 1: # Jakarta
         kota = 'JAKARTA'
         harga = 1500
 
-    elif tujuan == 2:
+    elif tujuan == 2: # Surabaya
         kota = 'SURABAYA'
         harga = 2000
 
-    elif tujuan == 3:
+    elif tujuan == 3: # Bandung
         kota = 'BANDUNG'
         harga = 2500
 
-    if pilihan == 2:
-        return kondisional_if(kota, harga, tiket, tujuan, bayar)
-    else:
-        return berulang(kota, harga)
+    if pilihan == 2: # kalkulasi harga tiket
+        return bayar_tujuan(kota, harga, tiket, tujuan, bayar)
+    else: # daftar harga tiket
+        return harga_tiket(kota, harga)
 
 print("""Menu
 1. Input Output
@@ -48,14 +65,14 @@ print("""Menu
 5. Keluar""")
 pilihan = int(input("Masukan pilihan anda "))
 
-if pilihan == 1:
+if pilihan == 1: # input/output
     a = int(input("Masukan Nilai a "))
     b = int(input("Masukan Nilai b "))
     c = int(input("Masukan Nilai c "))
 
-    put(a, b, c)
+    kalkulasi_operasi(a, b, c)
 
-if pilihan == 2:
+elif pilihan == 2: # kondisional if
     print("""KETERANGAN PEMBELIAN TIKET
 KODE TUJUAN -> 1. JAKARTA    Harga Rp. 1.500 dlm ribuan
                2. SURABAYA   Harga Rp. 2.000 dlm ribuan
@@ -69,16 +86,16 @@ KODE BAYAR  -> 1. REGULER    -> Harga Normal
 
     proses_info(tujuan, tiket, bayar)
 
-elif pilihan == 3:
+elif pilihan == 3: # bilangan berulang
     tujuan = int(input("Masukan Kode Tujuan	: "))
 
-    proses_info(tujuan, 0, 0)
+    proses_info(tujuan, 0, 0) # parameter 2 dan 3 tidak terpakai
 
-elif pilihan == 4:
+elif pilihan == 4: # loop dalam loop
     raise Exception("Opsi %d belum diimplementasikan" %(pilihan))
 
-elif pilihan == 5:
+elif pilihan == 5: # keluar aplikasi
     quit()
 
-else:
+else: # invalid
     raise ValueError("Opsi %d adalah tidak valid" %(pilihan))
